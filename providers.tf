e3769f39c689599c4270b5afb74de3d31ddadd33
root <root@ip-172-31-18-141.ec2.internal>
2022-05-25 03:50:27 +0000

    first commit

---
+++ providers.tf
@@ -0,0 +1,7 @@
+provider "azurerm"{
+#version = "2.39.0"
+subscription_id= "0c516c3d-d774-46ac-b31a-8b4d49bb4649"
+tenant_id= "7c572ed8-cfda-41f0-9085-a829875a97e2"
+client_id= "8567da40-0834-4985-b979-786a31947ad2"
+features{}
+}
